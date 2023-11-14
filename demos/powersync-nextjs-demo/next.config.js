6a15769043f7a80588df9ff5fc10d3e60543a03f
Steven Ontong <steven@journeyapps.com>
2023-11-14 14:21:10 +0200

    initial PWA

--- demos/powersync-nextjs-demo/next.config.js
+++ demos/powersync-nextjs-demo/next.config.js
@@ -1,12 +1,16 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  runtimeCaching: [
+    { handler: 'CacheFirst', urlPattern: '/views/todo-lists' },
+    { handler: 'CacheFirst', urlPattern: '/views/sql-console' }
+  ]
+});
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  webpack(config) {
-    config.resolve.alias = {
-      ...config.resolve.alias
-    };
-    return config;
-  }
-};
+const nextConfig = withPWA({
+  reactStrictMode: false
+});
 
 module.exports = nextConfig;
