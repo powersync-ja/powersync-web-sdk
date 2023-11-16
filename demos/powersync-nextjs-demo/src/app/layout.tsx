2bf731157aac506bb123847637e9cb99dcaa7f03
Steven Ontong <steven@journeyapps.com>
2023-11-16 16:25:40 +0200

    update pwa icons

--- demos/powersync-nextjs-demo/src/app/layout.tsx
+++ demos/powersync-nextjs-demo/src/app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from 'next';
 import { Rubik } from 'next/font/google';
 import './globals.css';
 import { ThemeProviderContainer } from '@/components/providers/ThemeProviderContainer';
-import SystemProvider from '@/components/providers/SystemProvider';
 import { DynamicSystemProvider } from '@/components/providers/DynamicSystemProvider';
 
 const rubik = Rubik({ subsets: ['latin'], display: 'swap' });
@@ -19,7 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#c44eff" />
-        <link rel="apple-touch-icon" href="/icon.png"></link>
+        <link rel="apple-touch-icon" href="/icons/icon.png"></link>
       </head>
       <body className={rubik.className}>
         <ThemeProviderContainer>
