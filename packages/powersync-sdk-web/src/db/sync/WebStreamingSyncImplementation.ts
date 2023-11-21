cecd4d7ea6899d64d7ef32a231a5bdf6da95d5ea
Steven Ontong <steven@journeyapps.com>
2023-11-21 09:17:18 -0700

    better strorage

--- packages/powersync-sdk-web/src/db/sync/WebStreamingSyncImplementation.ts
+++ packages/powersync-sdk-web/src/db/sync/WebStreamingSyncImplementation.ts
@@ -10,27 +10,17 @@ export type StateUpdatePayload = {
   lastSyncedAt?: string;
 };
 
-const CONNECTED_STORAGE_KEY = 'powersync-connected';
-const LAST_SYNCED_KEY = 'powersync-last-synced';
+const SYNC_STATUS_KEY = 'powersync-status';
 
 export class WebStreamingSyncImplementation extends AbstractStreamingSyncImplementation {
   constructor(options: AbstractStreamingSyncImplementationOptions) {
     super(options);
 
-    // TODO clean this up better
-    const isConnected = Boolean(localStorage.getItem(CONNECTED_STORAGE_KEY));
-    const lastSyncedString = localStorage.getItem(LAST_SYNCED_KEY);
-    const lastSyncedAt = lastSyncedString ? new Date(lastSyncedString) : undefined;
-    _.defer(() => this.updateSyncStatus(isConnected, lastSyncedAt));
-
+    this.loadSerializedState(localStorage.getItem(SYNC_STATUS_KEY));
     window.addEventListener('storage', (event) => {
-      switch (event.key) {
-        case CONNECTED_STORAGE_KEY:
-          this.updateSyncStatus(Boolean(event.newValue));
-          break;
-        case LAST_SYNCED_KEY:
-          this.updateSyncStatus(this.isConnected, new Date(event.newValue!));
-          break;
+      const { key, newValue } = event;
+      if (key == SYNC_STATUS_KEY && newValue) {
+        this.loadSerializedState(newValue);
       }
     });
   }
@@ -48,9 +38,23 @@ export class WebStreamingSyncImplementation extends AbstractStreamingSyncImpleme
   protected updateSyncStatus(connected: boolean, lastSyncedAt?: Date | undefined): void {
     super.updateSyncStatus(connected, lastSyncedAt);
     // persist to local storage to share between tabs
-    localStorage.setItem(CONNECTED_STORAGE_KEY, connected.toString());
-    if (lastSyncedAt) {
-      localStorage.setItem(LAST_SYNCED_KEY, lastSyncedAt.toISOString());
+    localStorage.setItem(
+      SYNC_STATUS_KEY,
+      JSON.stringify({
+        connected: this._isConnected,
+        lastSyncedAt: this._lastSyncedAt
+      })
+    );
+  }
+
+  /**
+   * Loads the shared sync status from a serialized string
+   */
+  protected loadSerializedState(state?: string | null) {
+    if (!state) {
+      return;
     }
+    const { connected, lastSyncedAt } = JSON.parse(state) as StateUpdatePayload;
+    _.defer(() => this.updateSyncStatus(connected, lastSyncedAt ? new Date(lastSyncedAt) : undefined));
   }
 }
